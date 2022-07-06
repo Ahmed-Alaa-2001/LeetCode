b47da268cb8a0969bb30c9f7e39ab8ff590c2d60
Ahmed-Alaa-2001 <63450270+Ahmed-Alaa-2001@users.noreply.github.com>
2022-07-06 17:24:56 +0200

    Time: 0 ms (100.00%), Space: 6 MB (18.24%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,15 +2,15 @@ class Solution {
 typedef long long ll;
 int dp[500];
 public:
-    ll DP(int n){
+    ll solve(int n){
         if(n<=1)return n;
         int &ret=dp[n];
         if(ret!=-1)return ret;
-        ret=DP(n-1)+DP(n-2);
+        ret=solve(n-1)+solve(n-2);
         return ret;
     }
     int fib(int n) {
         memset(dp,-1,sizeof dp);
-        return DP(n);
+        return solve(n);
     }
 };

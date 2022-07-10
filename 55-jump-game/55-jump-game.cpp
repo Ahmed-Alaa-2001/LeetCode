c6698a6cc67a46784ec3fa08e5f3058d3db366d8
Ahmed-Alaa-2001 <63450270+Ahmed-Alaa-2001@users.noreply.github.com>
2022-07-10 07:45:47 +0200

    Time: 1247 ms (7.69%), Space: 49.9 MB (13.01%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -3,11 +3,12 @@ typedef long long ll;
 ll dp[100005];
 public:
     bool solve(ll i,vector<int>& a){
-        if(i==a.size()-1)return dp[i]=true;
+        if(i==a.size()-1)return true;
         else if(i>=a.size())return false;
-        if(dp[i]!=-1)return dp[i];
-        for(int j=1;j<=a[i];j++)if(solve(i+j,a)) return dp[i]=true;
-        return dp[i]=false;
+        ll &ret=dp[i];
+        if(~ret)return ret;
+        for(int j=1;j<=a[i];j++)if(solve(i+j,a)) return ret=true;
+        return ret=false;
     }
     bool canJump(vector<int>& a) {
         memset(dp,-1,sizeof dp);

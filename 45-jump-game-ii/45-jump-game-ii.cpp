3fafafbbd63c0f17d524d847d299810da69e3d83
Ahmed-Alaa-2001 <63450270+Ahmed-Alaa-2001@users.noreply.github.com>
2022-07-10 07:59:07 +0200

    Time: 636 ms (19.76%), Space: 18.3 MB (6.12%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,16 +1,15 @@
 class Solution {
 typedef long long ll;
 ll dp[100005];
-ll mn=INT_MAX;
 public:
     ll solve(ll i,vector<int>& a){
         if(i==a.size()-1)return 0;
         else if(i>=a.size())return 0;
         ll &ret=dp[i];
         if(~ret)return ret;
-        ret=INT_MAX;
-        for(int j=1;j<=a[i];j++)ret=min(ret,solve(i+j,a)+1);
-        return ret;
+        ll mn=INT_MAX;
+        for(int j=1;j<=a[i];j++)mn=min(mn,solve(i+j,a)+1);
+        return ret=mn;
     }
     int jump(vector<int>& a) {
         memset(dp,-1,sizeof dp);

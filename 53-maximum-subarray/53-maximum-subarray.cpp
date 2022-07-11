85b2b3712cd71dc3469cee5d78094e2bcb56470f
Ahmed-Alaa-2001 <63450270+Ahmed-Alaa-2001@users.noreply.github.com>
2022-07-11 06:37:11 +0200

    Time: 173 ms (44.41%), Space: 67.8 MB (53.39%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,12 +1,12 @@
 class Solution {
-    typedef long long ll;
+typedef long long ll;
 public:
     int maxSubArray(vector<int>& a) {
-        ll vl=a[0],mx=a[0];
+        ll sum=a[0],mx=a[0];
         for(ll i=1;i<a.size();i++){
-            if(vl+a[i]<a[i])vl=a[i];
-            else vl+=a[i];
-            mx=max(mx,vl);
+            if(sum+a[i]<a[i])sum=a[i];
+            else sum+=a[i];
+            mx=max(mx,sum);
         }
         return mx;
     }

d32797d5a8374d57f6f6b51cd6b751a3d9879965
Ahmed-Alaa-2001 <63450270+Ahmed-Alaa-2001@users.noreply.github.com>
2022-07-15 11:11:30 +0200

    Time: 15 ms (32.13%), Space: 16 MB (10.55%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    int dp[100005][2];
+    int solve(int i,int buy,vector<int>& a){
+        if(i>=a.size())return 0;
+        int &ret=dp[i][buy];
+        if(~ret)return ret;
+        if(buy){
+            int choose1=solve(i+1,0,a)-a[i];
+            int choose2=solve(i+1,1,a);
+            ret=max(choose1,choose2);
+        }
+        else{
+            int choose3=solve(i+1,1,a)+a[i];
+            int choose4=solve(i+1,0,a);
+            ret=max(choose3,choose4);
+        }
+        return ret;
+    }
+public:
+    int maxProfit(vector<int>& prices) {
+        memset(dp,-1,sizeof dp);
+        return solve(0,1,prices);
+    }
+};

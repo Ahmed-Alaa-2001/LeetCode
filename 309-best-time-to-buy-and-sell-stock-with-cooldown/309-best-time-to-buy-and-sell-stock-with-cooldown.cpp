6c1552f75059d582df29e90e984c9f80cda24238
Ahmed-Alaa-2001 <63450270+Ahmed-Alaa-2001@users.noreply.github.com>
2022-07-15 11:15:46 +0200

    Time: 3 ms (91.22%), Space: 11.5 MB (55.83%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -5,20 +5,20 @@ public:
         if(i>=a.size()) return 0;
         int &ret=dp[i][buy];
         if(~ret)return ret;
-        if(buy==0){
-            int buyy=solve(i+1,1,a)-a[i];
-            int notbuy=solve(i+1,0,a);
-            ret=max(buyy,notbuy);
+        if(buy){
+            int choose1=solve(i+1,0,a)-a[i];
+            int choose2=solve(i+1,1,a);
+            ret=max(choose1,choose2);
         }
-        if(buy==1){
-            int sell=solve(i+2,0,a)+a[i];
-            int notsell=solve(i+1,1,a);
-            ret=max(sell,notsell);
+        if(!buy){
+            int choose3=solve(i+2,1,a)+a[i];
+            int choose4=solve(i+1,0,a);
+            ret=max(choose3,choose4);
         }
         return ret;
     }
     int maxProfit(vector<int>& prices) {
         memset(dp,-1,sizeof dp);
-        return solve(0,0,prices);
+        return solve(0,1,prices);
     }
 };

515ea799d478dde8985072966d93431da4fc33e9
Ahmed-Alaa-2001 <63450270+Ahmed-Alaa-2001@users.noreply.github.com>
2022-07-16 03:56:55 +0200

    Time: 12 ms (90.67%), Space: 15.7 MB (5.77%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    int bin(int i,vector<int>& numbers, int target){
-        int l=i+1,r=numbers.size()-1,md;
+    int solve(int i,int t,vector<int>& a){
+        int l=i+1,r=a.size()-1,md;
         while(l<=r){
             md=(l+r)/2;
-            if(numbers[md]+numbers[i]<target)l=md+1;
-            else if(numbers[md]+numbers[i]>target)r=md-1;
-            else return md;
+            if(a[md]+a[i]>t)r=md-1;
+            else if(a[md]+a[i]<t)l=md+1;
+            else{return md+1;}
         }
-        return md;
+        return 0;
     }
-
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int ans,i;
-        vector<int>a;
-        for(i=0;i<numbers.size();i++){
-            ans=bin(i,numbers,target);
-            if(numbers[ans]+numbers[i]==target){a.push_back(i+1);a.push_back(ans+1);break;}
+    vector<int> twoSum(vector<int>& a, int t) {
+        vector<int>b;
+        for(int i=0;i<a.size();i++){
+            int ans=solve(i,t,a);
+            if(ans){
+                b.push_back(i+1);
+                b.push_back(ans);
+                return b;
+            } 
         }
-        return a;
+        return b;
     }
 };
